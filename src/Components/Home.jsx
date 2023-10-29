5a93690bb6a4ce1f6b2877ff1ad5b47057912dd5
Sonsawan Ngamsom <86834525+murphy6867@users.noreply.github.com>
2023-10-29 16:12:35 +0700

    Add style header main

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,34 @@
+import { FiBell } from 'react-icons/fi';
+import { MdDownloading } from 'react-icons/md';
+import { BiUserCircle } from 'react-icons/bi';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
 const Home = () => {
   return (
-    <div className="w-full h-full bg-amber-500">Home</div>
+    <main className="w-full h-full bg-[#0B1D16] rounded-lg p-4">
+      <header className="w-full h-12 flex justify-between items-center bg-blue-500">
+        <div className='w-20 h-full flex items-center justify-center gap-2 ms-1'>
+          <button className='w-8 h-8 flex items-center justify-center text-xl bg-black rounded-full text-stone-400'>
+            <IoIosArrowBack className='text-2xl' />
+          </button>
+          <button className='w-8 h-8 flex items-center justify-center text-xl bg-black rounded-full text-stone-400'>
+            <IoIosArrowForward className='text-2xl' />
+          </button>
+        </div>
+        <div className='w-56 h-full flex gap-2 items-center'>
+          <div className='flex justify-center items-center text-white bg-black px-2 py-[5px] rounded-2xl cursor-pointer'>
+            <MdDownloading className='font-lg text-lg hover:text-[22px]' />
+            <p className='text-xs hover:text-[14px] font-semibold ms-1'>Install App</p>
+          </div>
+          <div className='w-7 h-7 flex items-center justify-center bg-black rounded-full cursor-pointer'>
+            <FiBell className='text-stone-300 text-lg hover:text-white' />
+          </div>
+          <div className='w-7 h-7 flex items-center justify-center bg-black rounded-full cursor-pointer'>
+            <BiUserCircle  className='w-full h-full text-stone-300 text-lg hover:text-white' />
+          </div>
+        </div>
+      </header>
+    </main>
   )
 }
 
--- src/Components/UserLibrary.jsx
+++ src/Components/UserLibrary.jsx
@@ -7,12 +7,12 @@ const UserLibrary = () => {
   return (
     <div className='w-full h-full text-white flex'>
         <div className="w-full h-8 flex items-center justify-between px-4">
-            <div>
-                <AiOutlineSearch className="text-2xl text-stone-400" />
+            <div className="p-[7px] cursor-pointer hover:bg-stone-800 rounded-full duration-300">
+                <AiOutlineSearch className="text-2xl text-stone-400 hover:text-white duration-300" />
             </div>
-            <div className="flex items-center">
-                <h1 className="text-sm font-sm text-stone-400">Recents</h1>
-                <GiHamburgerMenu className="text-lg text-stone-400 mx-1" />
+            <div className="flex items-center p-[7px] cursor-pointer">
+                <h1 className="text-sm font-sm text-stone-400 hover:text-white duration-200 hover:text-base">Recents</h1>
+                <GiHamburgerMenu className="text-lg text-stone-400 mx-1 hover:text-white duration-200 hover:text-xl" />
             </div>
         </div>
         <div>
